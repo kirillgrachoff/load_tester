2310e83736481679a79351427b8daa6f63eaeecd
Kirill Grachev <kirill.grachoff@gmail.com>
2022-10-30 18:41:15 +0300

    Show version with Cobra package

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os/signal"
 	"syscall"
@@ -10,20 +9,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var versionFlag bool
-
-const version = "1.1.0"
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "load_tester",
-	Short: "Simple app for load test",
-	Long:  "Sends many Get requests to an address",
-	Run: func(cmd *cobra.Command, args []string) {
-		if versionFlag {
-			fmt.Println(version)
-		}
-	},
+	Use:     "load_tester",
+	Short:   "Simple app for load test",
+	Long:    "Sends many Get requests to an address",
+	Version: "1.1.1",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -35,8 +26,4 @@ func Execute() {
 	if err != nil {
 		log.Printf("error: %s", err)
 	}
-}
-
-func init() {
-	rootCmd.Flags().BoolVar(&versionFlag, "version", false, "show version")
 }
